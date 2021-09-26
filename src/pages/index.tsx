79c6a3b64d1437135a08b8fd29294f42b88f53ca
Vitor Balocco <vitorbal@gmail.com>
2021-09-26 23:53:19 +0200

    Add a bit of styling to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,30 @@ import GameSalesGraph from "../components/GameSalesGraph";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Video Games</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>Video Games Sales</h1>
-        <GameSalesGraph />
-      </main>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <main>
+          <h1 className="text-center text-5xl">Games! Games! Games!</h1>
+          <div style={{ width: "800px" }}>
+            <GameSalesGraph />
+          </div>
+        </main>
 
-      <footer>
-        <a
-          href="https://twitter.com/vitorbal"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Made by Vitor Balocco
-        </a>
-      </footer>
-    </div>
+        <footer>
+          <a
+            href="https://twitter.com/vitorbal"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Made by Vitor Balocco
+          </a>
+        </footer>
+      </div>
+    </>
   );
 }
